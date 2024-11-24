ec5bb2b1bab9d74a946e5a52ff4c1384e91d0be7
helpmonster <emmanuelonosode4@gmail.com>
2024-11-24 15:38:24 +0100

    someshit

---
+++ script.js
@@ -0,0 +1,10 @@
+function openav() {
+ const that = document.querySelector(".side-nav")
+ that.style.width = "250px";
+ 
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.querySelector(".side-nav").style.width = "0";
+}
